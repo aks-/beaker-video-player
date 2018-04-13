102654d981f2ae337259d816caf0939b998d0469
Ashok <coderatlabs@gmail.com>
2018-04-13 16:17:11 +0530

    add example

--- example/script.js
+++ example/script.js
@@ -1,4 +1,4 @@
-const player  =  require('./')
+const player  =  require('../')
 var url = 'dat://1047fec2e6c4bc9756811a768396912ce89af5f691b7f40e991440bbabad3d10/playlist.m3u'
 var o = player(url)
 
